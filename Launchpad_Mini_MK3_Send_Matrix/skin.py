2f5cbde7a23023d5120952a7b735884602523ef5
Jonnie <45931007+jonniepeller@users.noreply.github.com>
2021-12-28 14:55:11 +1100

    Lint and rename

--- Launchpad_Mini_MK3_Send_Matrix/skin.py
+++ Launchpad_Mini_MK3_Send_Matrix/skin.py
@@ -1,4 +1,4 @@
-#Embedded file name: /Users/versonator/Jenkins/live/output/Live/mac_64_static/Release/python-bundle/MIDI Remote Scripts/Launchpad_Mini_MK3/skin.py
+# Embedded file name: /Users/versonator/Jenkins/live/output/Live/mac_64_static/Release/python-bundle/MIDI Remote Scripts/Launchpad_Mini_MK3/skin.py
 from __future__ import absolute_import, print_function, unicode_literals
 from builtins import object
 from ableton.v2.control_surface import merge_skins, Skin
@@ -6,10 +6,9 @@ from ableton.v2.control_surface.elements import Color
 from novation.colors import Rgb
 from novation.skin import skin as base_skin
 
+
 class Colors(object):
-
     class Mode(object):
-
         class Session(object):
             Launch = Color((Rgb.PALE_GREEN_HALF.midi_value, Rgb.WHITE_HALF.midi_value))
             Overview = Color((Rgb.BLUE.midi_value, Rgb.WHITE_HALF.midi_value))
