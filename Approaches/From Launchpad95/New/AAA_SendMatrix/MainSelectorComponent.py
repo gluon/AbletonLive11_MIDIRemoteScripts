a83daa54d30cecaf64553d99f1485a0853abaf45
Jonnie <45931007+jonniepeller@users.noreply.github.com>
2021-12-29 16:46:02 +1100

    No errors but not working

--- Approaches/From Launchpad95/New/AAA_SendMatrix/MainSelectorComponent.py
+++ Approaches/From Launchpad95/New/AAA_SendMatrix/MainSelectorComponent.py
@@ -4,12 +4,8 @@ from _Framework.ModeSelectorComponent import ModeSelectorComponent
 from _Framework.ButtonElement import ButtonElement
 from _Framework.ButtonMatrixElement import ButtonMatrixElement
 from .SpecialSessionComponent import SpecialSessionComponent
-from .SendsSelectorComponent import SendsSelectorComponent
-
-try:
-    exec("from .Settings import Settings")
-except ImportError:
-    exec("from .Settings import *")
+from .SendsMixerComponent import SendsMixerComponent
+from .Settings import Settings
 
 
 class MainSelectorComponent(ModeSelectorComponent):
@@ -48,12 +44,9 @@ class MainSelectorComponent(ModeSelectorComponent):
         # initialize index variables
         self._mode_index = 0  # Inherited from parent
         self._main_mode_index = 0  # LP original modes
-        self._sub_mode_list = [0, 0, 0, 0]
-        for index in range(4):
-            self._sub_mode_list[index] = 0
         self.set_mode_buttons(self._mode_buttons)
 
-        # Create session
+        # initialise session
         self._session = SpecialSessionComponent(
             matrix.width(), matrix.height(), None, self._control_surface, self
         )
@@ -64,14 +57,16 @@ class MainSelectorComponent(ModeSelectorComponent):
         for button in self._side_buttons + self._nav_buttons:
             self._all_buttons.append(button)
 
-        self._sub_modes = SendsSelectorComponent(
-            matrix, side_buttons, self._session, self._control_surface
-        )
-        self._sub_modes.name = "Mixer_Modes"
-        self._sub_modes.set_update_callback(self._update_control_channels)
-
         self._init_session()
         self._all_buttons = tuple(self._all_buttons)
+
+        # Initialise mixer
+        self._mixer = SendsMixerComponent(matrix.width())
+        self._mixer.name = "Mixer"
+        self._mixer.selected_strip().name = "Selected_Channel_strip"
+        for column in range(matrix.width()):
+            self._mixer.channel_strip(column).name = "Channel_Strip_" + str(column)
+        self._session.set_mixer(self._mixer)
 
     def disconnect(self):
         for button in self._modes_buttons:
@@ -86,37 +81,24 @@ class MainSelectorComponent(ModeSelectorComponent):
         self._side_buttons = None
         self._nav_buttons = None
         self._config_button = None
+        self._mixer = None
         ModeSelectorComponent.disconnect(self)
 
     def session_component(self):
         return self._session
 
     def _update_mode(self):
-        mode = self._modes_heap[-1][
-            0
-        ]  # get first value of last _modes_heap tuple. _modes_heap tuple structure is (mode, sender, observer)
-
+        # Get the mode (the first value of last _modes_heap tuple, where _modes_heap tuple structure is (mode, sender, observer))
+        mode = self._modes_heap[-1][0]
         self.log_message(f"Mode: {mode}")
-        assert mode in range(self.number_of_modes())  # 8 for this script
 
         # TODO Come back here and figure out what this is doing
         if self._main_mode_index == mode:
-            if (
-                self._main_mode_index == 1
-            ):  # user mode 1 and device controller and instrument mode
-                self._sub_mode_list[self._main_mode_index] = (
-                    self._sub_mode_list[self._main_mode_index] + 1
-                ) % len(Settings.USER_MODES_1)
+            # Mixer mode
+            if self._main_mode_index == 3:
                 self.update()
-            elif self._main_mode_index == 2:  # user mode 2  and step sequencer
-                self._sub_mode_list[self._main_mode_index] = (
-                    self._sub_mode_list[self._main_mode_index] + 1
-                ) % len(Settings.USER_MODES_2)
-                self.update()
-            elif self._main_mode_index == 3:  # Mixer mode
-                self.update()
-            else:  # Session mode
-                self._sub_mode_list[self._main_mode_index] = 0
+            # Session mode
+            else:
                 self._mode_index = 0
 
         else:
@@ -127,77 +109,17 @@ class MainSelectorComponent(ModeSelectorComponent):
         self._clean_heap()
         self._modes_heap = [(mode, None, None)]
 
-    def number_of_modes(self):
-        return 1 + 3 + 3 + 1
-
     def on_enabled_changed(self):
         self.update()
 
     def _update_mode_buttons(self):
         self._modes_buttons[0].set_on_off_values("Mode.Session.On", "Mode.Session.Off")
-        self._modes_buttons[3].set_on_off_values("Mode.Mixer.On", "Mode.Mixer.Off")
-        mode1 = self.getSkinName(Settings.USER_MODES_1[self._sub_mode_list[1]])
-        mode2 = self.getSkinName(Settings.USER_MODES_2[self._sub_mode_list[2]])
-        self._modes_buttons[1].set_on_off_values(
-            "Mode." + mode1 + ".On", "Mode." + mode1 + ".Off"
-        )
-        self._modes_buttons[2].set_on_off_values(
-            "Mode." + mode2 + ".On", "Mode." + mode2 + ".Off"
-        )
 
         for index in range(4):
             if index == self._main_mode_index:
                 self._modes_buttons[index].turn_on()
             else:
                 self._modes_buttons[index].turn_off()
-
-    def getSkinName(self, user2Mode):
-        if user2Mode == "instrument":
-            user2Mode = "Note"
-        if user2Mode == "device":
-            user2Mode = "Device"
-        if user2Mode == "user 1":
-            user2Mode = "User"
-        if user2Mode == "user 2":
-            user2Mode = "User2"
-        if user2Mode == "drum stepseq":
-            user2Mode = "StepSequencer"
-        if user2Mode == "melodic stepseq":
-            user2Mode = "StepSequencer2"
-        return user2Mode
-
-    def channel_for_current_mode(self):
-        # in this code, midi channels start at 0.
-        # so channels range from 0 - 15.
-        # mapping to 1-16 in the real world
-
-        if self._main_mode_index == 0:
-            new_channel = 0  # session
-
-        elif self._main_mode_index == 1:
-            if self._sub_mode_list[self._main_mode_index] == 0:
-                new_channel = 11  # instrument controller
-                # instrument controller uses base channel plus the 4 next ones. 11,12,13,14,15
-                if self._instrument_controller != None:
-                    self._instrument_controller.base_channel = new_channel
-            elif self._sub_mode_list[self._main_mode_index] == 1:
-                new_channel = 3  # device controller
-            elif self._sub_mode_list[self._main_mode_index] == 2:
-                new_channel = 4  # plain user mode 1
-
-        elif self._main_mode_index == 2:
-            if self._sub_mode_list[self._main_mode_index] == 0:
-                new_channel = 1  # step seq
-            elif self._sub_mode_list[self._main_mode_index] == 1:
-                new_channel = 2  # melodic step seq
-            elif self._sub_mode_list[self._main_mode_index] == 2:
-                new_channel = 5  # plain user mode 2
-
-        elif self._main_mode_index == 3:  # mixer modes
-            # mixer uses base channel 7 and the 4 next ones.
-            new_channel = 6 + self._sub_modes.mode()  # 6,7,8,9,10
-
-        return new_channel
 
     def update(self):
         assert self._modes_buttons != None
@@ -207,19 +129,33 @@ class MainSelectorComponent(ModeSelectorComponent):
 
             as_active = True
             as_enabled = True
+            self._mixer.set_allow_update(False)
             self._session.set_allow_update(False)
-            self._config_button.send_value(
-                40
-            )  # Set LP double buffering mode (investigate this)
-            self._config_button.send_value(1)  # Set LP X-Y layout grid mapping mode
+
+            # Set LP double buffering mode (investigate this)
+            self._config_button.send_value(40)
+            # Set LP X-Y layout grid mapping mode
+            self._config_button.send_value(1)
 
             if self._main_mode_index == 0:
                 # session
-                self._control_surface.show_message("SESSION MODE")
+                self._control_surface.show_message("Sends matrix mode")
                 self._setup_session(as_active, as_enabled)
                 self._update_control_channels()
                 self._mode_index = 0
 
+            for index in range(len(self._modes_buttons)):
+                button = self._modes_buttons[index]
+                if index == 2:
+                    button.set_on_off_values("Mixer.Sends")
+                elif index == 3:
+                    button.set_on_off_values("Mixer.Sends")
+                if index == self._mode_index:
+                    button.turn_off()
+                else:
+                    button.turn_on()
+
+            self._mixer.set_allow_update(True)
             self._session.set_allow_update(True)
 
     def _setup_session(self, as_active, as_navigation_enabled):
@@ -235,49 +171,32 @@ class MainSelectorComponent(ModeSelectorComponent):
         if as_active:
             self._activate_navigation_buttons(True)
             self._activate_matrix(True)
-            if self._sub_modes.is_enabled():  # go back to default mode
-                self._sub_modes.set_mode(-1)
-            else:
-                self._sub_modes.release_controls()
+            for track in range(self._matrix.width()):
+                for row in range(self._matrix.height()):
+                    self._matrix.get_button(track, row).set_on_off_values(
+                        127, "DefaultButton.Disabled"
+                    )
 
-        self._sub_modes.set_enabled(as_active)
+                strip = self._mixer.channel_strip(track)
+                strip.set_default_buttons(None)
+                strip.set_send_controls((None, None))
 
-        # TODO This is iterating through all scenes and tracks—We want to make a send button on each track / send combo
-        # matrix
         self._activate_matrix(True)
-        for scene_index in range(self._session._num_scenes):  # iterate over scenes
-            scene = self._session.scene(scene_index)
-            # if as_active:  # set scene launch buttons
-            #     scene_button = self._side_buttons[scene_index]
-            #     scene_button.set_enabled(as_active)
-            #     scene_button.set_on_off_values(
-            #         "DefaultButton.Disabled", "DefaultButton.Disabled"
-            #     )
-            #     scene.set_launch_button(scene_button)
-            # else:
-            #     scene.set_launch_button(None)
+        # iterate over tracks
+        for track_index in range(self._session._num_tracks):
+            if as_active:
+                for send_index in range(self._matrix.height()):
+                    button = self._matrix.get_button(track_index, send_index)
+                    strip = self._mixer.channel_strip(track)
+                    button.set_on_off_values("Mixer.Sends")
+                    button.set_enabled(as_active)
 
-            for track_index in range(
-                self._session._num_tracks
-            ):  # iterate over tracks of a scene -> clip slots
-                if as_active:  # set clip slot launch button
-                    button = self._matrix.get_button(track_index, scene_index)
-                    # button.set_on_off_values(
-                    #     "DefaultButton.Disabled", "DefaultButton.Disabled"
-                    # )
-                    # button.set_enabled(as_active)
-                    # scene.clip_slot(track_index).set_launch_button(button)
-                else:
-                    scene.clip_slot(track_index).set_launch_button(None)
+                    strip.set_default_buttons(button)
+                    button.force_next_send()
+                    button.turn_off()
 
-                # FOR each SCENE??
-
-        if as_navigation_enabled:  # track nav buttons (don't need scene nav)
-            self._session.set_track_bank_buttons(
-                self._nav_buttons[3], self._nav_buttons[2]
-            )
-        else:
-            self._session.set_track_bank_buttons(None, None)
+        # Always have track nav buttons (don't need scene nav because we're not working with clips)
+        self._nav_buttons[3], self._nav_buttons[2]
 
     def _init_session(self):
         session_height = self._matrix.height()
@@ -306,7 +225,22 @@ class MainSelectorComponent(ModeSelectorComponent):
 
     # Update the channels of the buttons in the user modes..
     def _update_control_channels(self):
-        new_channel = self.channel_for_current_mode()
+        new_channel = 0
         for button in self._all_buttons:
             button.set_channel(new_channel)
             button.force_next_send()
+
+    def _setup_send_mode(self, send_number):
+        for track in range(self._matrix.width()):
+            strip = self._mixer.channel_strip(track)
+            strip.set_default_buttons(None)
+            for row in range(self._matrix.height()):
+                self._matrix.get_button(track, row).set_on_off_values(
+                    f"Mixer.SendsSlider_{send_number}"
+                )
+
+        # TODO set button as slider
+
+        # self._session.set_stop_track_clip_buttons(None)
+        # self._session.set_stop_all_clips_button(None)
+        # self._mixer.set_global_buttons(None, None, None)
--- Approaches/From Launchpad95/New/AAA_SendMatrix/SendsChannelStripComponent.py
+++ Approaches/From Launchpad95/New/AAA_SendMatrix/SendsChannelStripComponent.py
@@ -5,28 +5,31 @@ from itertools import chain
 
 
 class SendsChannelStripComponent(ChannelStripComponent):
-
-    """Subclass of channel strip component offering defaultbuttons for the timeables"""
-
     def __init__(self):
         ChannelStripComponent.__init__(self)
-        self._default_send1_button = None
-        self._default_send2_button = None
+        self._default_send_buttons = {}
 
     def disconnect(self):
         """releasing references and removing listeners"""
         if self._track != None:
             sends = self._track.mixer_device.sends
-            if len(sends) > 0 and sends[0].value_has_listener(self._on_send1_changed):
-                sends[0].remove_value_listener(self._on_send1_changed)
-            if len(sends) > 1 and sends[1].value_has_listener(self._on_send2_changed):
-                sends[1].remove_value_listener(self._on_send2_changed)
-        if self._default_send1_button != None:
-            self._default_send1_button.remove_value_listener(self._default_send1_value)
-            self._default_send1_button = None
-        if self._default_send2_button != None:
-            self._default_send2_button.remove_value_listener(self._default_send2_value)
-            self._default_send2_button = None
+            for send in sends:
+                if sends[0].value_has_listener(self._on_send0_changed):
+                    sends[0].remove_value_listener(self._on_send0_changed)
+                if sends[1].value_has_listener(self._on_send1_changed):
+                    sends[1].remove_value_listener(self._on_send1_changed)
+                # TODO Add more sends
+        for (
+            default_send_button_idx,
+            default_send_button,
+        ) in self._default_send_buttons:
+            if default_send_button != None:
+                self._default_send_buttons[
+                    default_send_button_idx
+                ].remove_value_listener(
+                    self._default_send_buttons[default_send_button_idx]
+                )
+                self._default_send_buttons[default_send_button_idx] = None
         ChannelStripComponent.disconnect(self)
 
     def set_track(self, track):
@@ -34,37 +37,52 @@ class SendsChannelStripComponent(ChannelStripComponent):
         if track != self._track:
             if self._track != None:
                 sends = self._track.mixer_device.sends
-                if len(sends) > 0 and sends[0].value_has_listener(
-                    self._on_send1_changed
-                ):
-                    sends[0].remove_value_listener(self._on_send1_changed)
-                if len(sends) > 1 and sends[1].value_has_listener(
-                    self._on_send2_changed
-                ):
-                    sends[1].remove_value_listener(self._on_send2_changed)
+                send_idx = 0
+                for send in sends:
+                    if send_idx == 0 and sends[send_idx].value_has_listener(
+                        self._on_send0_changed
+                    ):
+                        sends[send_idx].remove_value_listener(self._on_send0_changed)
+                    elif send_idx == 1 and sends[send_idx].value_has_listener(
+                        self._on_send1_changed
+                    ):
+                        sends[send_idx].remove_value_listener(self._on_send1_changed)
+                    send_idx += 1
+                # TODO Add more sends
             ChannelStripComponent.set_track(self, track)
         else:
             self.update()
 
-    def set_default_buttons(self, send1, send2):
-        assert (send1 == None) or isinstance(send1, ConfigurableButtonElement)
-        assert (send2 == None) or isinstance(send2, ConfigurableButtonElement)
-        if send1 != self._default_send1_button:
-            if self._default_send1_button != None:
-                self._default_send1_button.remove_value_listener(
-                    self._default_send1_value
+    def set_default_buttons(self, *send_buttons):
+        if (
+            send_buttons is not None
+            and len(send_buttons) > 0
+            and self._default_send_buttons is not None
+            and len(self._default_send_buttons) > 0
+        ):
+            self._control_surface.log_message("It do happen!")
+            send_button_idx = 0
+            for send_button in send_buttons:
+                assert (send_button == None) or isinstance(
+                    send_button, ConfigurableButtonElement
                 )
-            self._default_send1_button = send1
-            if self._default_send1_button != None:
-                self._default_send1_button.add_value_listener(self._default_send1_value)
-        if send2 != self._default_send2_button:
-            if self._default_send2_button != None:
-                self._default_send2_button.remove_value_listener(
-                    self._default_send2_value
-                )
-            self._default_send2_button = send2
-            if self._default_send2_button != None:
-                self._default_send2_button.add_value_listener(self._default_send2_value)
+                if send_button != self._default_send_buttons[send_button_idx]:
+                    if self._default_send_buttons[send_button_idx] != None:
+                        self._default_send_buttons[
+                            send_button_idx
+                        ].remove_value_listener()
+                    self._default_send_buttons[send_button_idx] = send_button
+                    if self._default_send_buttons[send_button_idx] != None:
+                        if send_button_idx == 0:
+                            self._default_send_buttons[
+                                send_button_idx
+                            ].add_value_listener(self._default_send0_value)
+                        elif send_button_idx == 1:
+                            self._default_send_buttons[
+                                send_button_idx
+                            ].add_value_listener(self._default_send1_value)
+                        # TODO Add more sends
+                send_button_idx += 1
         self.update()
 
     def set_send_controls(self, controls):
@@ -84,68 +102,68 @@ class SendsChannelStripComponent(ChannelStripComponent):
             if self.is_enabled():
                 if self._track != None:
                     sends = self._track.mixer_device.sends
-                    if len(sends) > 0:
-                        if not sends[0].value_has_listener(self._on_send1_changed):
-                            sends[0].add_value_listener(self._on_send1_changed)
-                        self._on_send1_changed()
-                    elif self._default_send1_button != None:
-                        self._default_send1_button.turn_off()
-                    if len(sends) > 1:
-                        if not sends[1].value_has_listener(self._on_send2_changed):
-                            sends[1].add_value_listener(self._on_send2_changed)
-                        self._on_send2_changed()
-                    elif self._default_send2_button != None:
-                        self._default_send2_button.turn_off()
-                else:
-                    if self._default_send1_button != None:
-                        self._default_send1_button.reset()
-                    if self._default_send2_button != None:
-                        self._default_send2_button.reset()
-                    if self._send_controls != None:
-                        for send_control in self._send_controls:
-                            if send_control != None:
-                                send_control.reset()
+                    send_idx = 0
+                    for send in sends:
+                        if send_idx == 0 and not send.value_has_listener(
+                            self._on_send0_changed
+                        ):
+                            send.add_value_listener(self._on_send0_changed)
+                        elif send_idx == 1 and not send.value_has_listener(
+                            self._on_send1_changed
+                        ):
+                            send.add_value_listener(self._on_send1_changed)
+                        self._on_send_changed(send_idx)
+                        send_idx += 1
 
-    def _default_send1_value(self, value):
-        assert self._default_send1_button != None
+                    # TODO turn off send buttons if they aren't sends there anymore!!
+                    # elif self._default_send0_button != None:
+                    #     self._default_send0_button.turn_off()
+                    # elif self._default_send1_button != None:
+                    #     self._default_send1_button.turn_off()
+                else:
+                    for default_send_button in self._default_send_buttons:
+                        if self.default_send_button != None:
+                            self.default_send_button.reset()
+                        if self._send_controls != None:
+                            for send_control in self._send_controls:
+                                if send_control != None:
+                                    send_control.reset()
+
+    def _default_send_value(self, send_number, value):
+        assert self._default_send_buttons[send_number] != None
         assert value in range(128)
         if self.is_enabled() and (
             (self._track != None) and (len(self._track.mixer_device.sends) > 0)
         ):
-            if (value != 0) or (not self._default_send1_button.is_momentary()):
-                send1 = self._track.mixer_device.sends[0]
-                if send1.is_enabled:
-                    send1.value = send1.default_value
+            if (value != 0) or (
+                not self._default_send_buttons[send_number].is_momentary()
+            ):
+                send = self._track.mixer_device.sends[send_number]
+                if send.is_enabled:
+                    send.value = send.default_value
 
-    def _default_send2_value(self, value):
-        assert self._default_send2_button != None
-        assert value in range(128)
-        if self.is_enabled() and (
-            (self._track != None) and (len(self._track.mixer_device.sends) > 1)
-        ):
-            if (value != 0) or (not self._default_send2_button.is_momentary()):
-                send2 = self._track.mixer_device.sends[1]
-                if send2.is_enabled:
-                    send2.value = send2.default_value
+    def _default_send0_value(self, value):
+        self._default_send_value(value, 0)
 
-    def _on_send1_changed(self):
+    def _default_send1_value(self, value):
+        self._default_send_value(value, 1)
+
+    # TODO add more sends
+
+    def _on_send_changed(self, send_number):
         assert self._track != None
         sends = self._track.mixer_device.sends
         assert len(sends) > 0
-        if self.is_enabled() and (self._default_send1_button != None):
-            send1 = sends[0]
-            if send1.value == send1.default_value:
-                self._default_send1_button.turn_on()
-            else:
-                self._default_send1_button.turn_off()
+        if self.is_enabled() and len(self._default_send_buttons) > send_number:
+            if self._default_send_buttons[send_number] != None:
+                send = sends[send_number]
+                if send.value == send.default_value:
+                    self._default_send_buttons[send_number].turn_on()
+                else:
+                    self._default_send_buttons[send_number].turn_off()
 
-    def _on_send2_changed(self):
-        assert self._track != None
-        sends = self._track.mixer_device.sends
-        assert len(sends) > 1
-        if self.is_enabled() and (self._default_send2_button != None):
-            send2 = sends[1]
-            if send2.value == send2.default_value:
-                self._default_send2_button.turn_on()
-            else:
-                self._default_send2_button.turn_off()
+    def _on_send0_changed(self):
+        self._on_send_changed(self, 0)
+
+    def _on_send1_changed(self):
+        self._on_send_changed(self, 0)
